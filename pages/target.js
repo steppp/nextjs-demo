613fd117b2344a90fbb9791615795be9e94de5c5
Stefano Andriolo <dev@srand.it>
2022-03-26 18:40:43 +0100

    Remove client-side code from target.js

--- pages/target.js
+++ pages/target.js
@@ -1,11 +1,7 @@
 import React from 'react'
-import { withDefaultPage } from "../src/compositors/withDefaultPage"
 
 export default function Target(props) {
-    const alertMessage = (document.referrer && `Previous path was ${document.referrer}`)
-
     return (
         <h1>Target page!</h1>
     )
-    // return <TargetContents {...props} alertMessage={alertMessage} />
 }
