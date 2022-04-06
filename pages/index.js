473a3171d9c0e2867f346750239d6a4f124cdde8
Stefano Andriolo <stefano.andriolo@caffeina.com>
2022-04-06 17:51:23 +0200

    Add an example of retrieving data from a JSON file

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,31 @@ import Link from 'next/link'
 import React from 'react'
 import styles from '../styles/Home.module.scss'
 import SlideFadeInOut from '../src/components/animations/SlideFadeInOut'
+import Head from 'next/head'
+import staticData from '../src/static/home.json'
 
 export default function Home(props) {
   return (
-    <SlideFadeInOut>
-      <div>
-        <h1 className={styles.title}>
-          My awesome <a href="https://nextjs.org" target="_blank" rel='noreferrer'>Next.js</a> website!
-        </h1>
+    <>
+      <Head>
+        <title>{staticData.title}</title>
+      </Head>
+      <SlideFadeInOut>
+        <div>
+          <h1 className={styles.title}>
+            My awesome <a href="https://nextjs.org" target="_blank" rel='noreferrer'>Next.js</a> website!
+          </h1>
 
-        <p className={styles.description}>
-          <Link href="/target">
-            <a>
-              Go to the target page
-            </a>
-          </Link>
-        </p>
-      </div>
-    </SlideFadeInOut>
+          <p className={styles.description}>
+            {staticData.description} <br />
+            <Link href="/target">
+              <a className={styles.internalLink}>
+                Go to the target page
+              </a>
+            </Link>
+          </p>
+        </div>
+      </SlideFadeInOut>
+    </>
   )
 }
